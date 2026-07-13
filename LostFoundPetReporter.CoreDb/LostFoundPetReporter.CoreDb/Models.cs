024c781c039a4821b9f248dd5b4127c93e79b23f
NaftaliProjects <naftalidavidov298@gmail.com>
2026-07-13 17:55:33 +0300

    Initial commit: Created CoreDb library, installed EF Core, and added Models.cs

---
+++ LostFoundPetReporter.CoreDb/LostFoundPetReporter.CoreDb/Models.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LostFoundPetReporter.CoreDb
+{
+    public class Models
+    {
+
+        public abstract class BaseModel
+        {
+            public int Id { get; set; }
+            public Byte[] TimeStamp { get; set; }
+        }
+
+        /// <summary>
+        /// Holds the profile and contact information for a system user.
+        /// </summary>
+        public class User : BaseModel
+        {
+            public String Name { get; set; }
+            public String Email { get; set; }
+            public String Phone { get; set; }
+
+        }
+
+        /// <summary>
+        /// Hold Description of a Pet in a LostReport or a FoundReport 
+        /// </summary>
+        public class AnimalDescription
+        {
+            public String Name { get; set; }
+            public String Color { get; set; }
+            public String Type { get; set; }
+            public String SubType { get; set; }
+        }
+
+        /// <summary>
+        /// SubModel for FoundReport to hold images data related to a FoundReport
+        /// </summary>
+        public class FoundReportExtFiles
+        {
+            public String FilePath { get; set; }
+            public String FileName { get; set; }
+            public String Description { get; set; }
+        }
+
+        /// <summary>
+        /// A found report from any user will be save in the appropriate LostReport
+        /// </summary>
+        public class FoundReport
+        {
+            public AnimalDescription LostPetDesc { get; set; }
+            public User FoundReporter { get; }
+            public String FoundCoordinates { get; set; }
+            public List<FoundReportExtFiles> Extfiles { get; set; }
+
+        }
+
+
+        /// <summary>
+        /// Lost report from users that lost their pets and need help in finding it
+        /// </summary>
+        public class LostReport
+        {
+            public AnimalDescription LostPetDesc { get; set; }
+            public User LostReporter { get; }
+            public String LastSeenCoordinates { get; set; }
+
+            public List<FoundReport> foundReports = new List<FoundReport>();
+        }
+    }
+}
